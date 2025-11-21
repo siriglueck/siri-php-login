b6ed6da0a1f153fd5cf242a52d436a5d47af03f6
SiriGlück <siriluck.rattanapong@gmail.com>
2025-11-21 17:43:37 +0000

    ADD: project structure

---
+++ public/header.php
@@ -0,0 +1,17 @@
+<?php
+    declare(strict_types=1);
+    //! die folgendern 2 Zeilen in der Produktiv-Variante löschen!
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
+    session_start();    
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learn Login</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php require_once 'header.php' ?>
+<body>
+    <h1>Learn Login</h1>
+</body>
+</html>
