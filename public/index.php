41bab5e8a57a66bbf46e2286a91dc27a0e55443c
SiriGlück <siriluck.rattanapong@gmail.com>
2025-11-21 22:08:06 +0000

    ADD: CSS and images

--- public/index.php
+++ public/index.php
@@ -9,14 +9,46 @@ require_once __DIR__ . '/header.php';
 $stmt = $pdo->query("SELECT * FROM users");
 $users = $stmt->fetchAll();
 
-foreach ($users as $user) {
-    echo $user->username . "<br>";
-    echo $user->created_at . "<br>";
-    echo $user->role . "<br>";
-    echo $user->profile_image . "<br>";
-}
 ?>
 <body>
-    <h1>Learn Login</h1>
+    <header>
+        <h1>Learn Login</h1>
+    </header>
+    <main class="container card">
+         <table>
+        <thead>
+          <tr>
+            <th>Img</th>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Role</th>
+            <th>Created</th>
+            <th>Updated</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+          <?php foreach ($users as $u): ?>
+            <tr>
+              <td><img src="..<?= ($u->profile_image)?>" class="icon" onerror="this.onerror=null; this.src='../uploads/user.png';" ></td>
+              <td><?= $u->id?></td>
+              <td><?= $u->username ?></td>
+              <td><?= $u->role?></td>
+              <td><?= $u->created_at?></td>
+              <td><?= $u->updated_at?></td>
+              <td><a href="edit.php?id=<?= (int)$u->id ?>" class="button">Bearbeiten</a></td>
+              <td><a href="edit.php?id=<?= (int)$u->id ?>" class="button text-danger">Löschen</a></td>
+                <!-- <form action="delete.php" style="display:inline;" method="post">
+                  <input type="hidden" name="id" value="<?= (int)$u->id ?>">
+                  <button type="submit" class="button text-danger">Löschen</button>
+                </form> -->
+              
+            </tr>
+          <?php endforeach; ?>
+        <tbody>
+
+        </tbody>
+      </table>
+    </main>
 </body>
 </html>
